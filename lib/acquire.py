3cb971665c26225001b064fc83f55ef0facf9d89
Tony Johnson <tonyj@stanford.edu>
2019-01-29 17:19:33 -0800

    initial refactor complete

--- lib/acquire.py
+++ lib/acquire.py
@@ -9,31 +9,44 @@ from pd import PhotodiodeReadout
 
 class TestCoordinator(object):
     ''' Base (abstract) class for all tests '''
-    def __init__(self, options):
+    def __init__(self, options, test_type, image_type):
         self.run = options.getInt('run')
         self.symlink = options['symlink']
+        self.test_type = test_type
+        self.image_type = image_type
         self.test_seq_num = 0
 
     def take_images(self):
         pass
 
-    def take_bias_images(self, count, test_type):
+    def take_bias_images(self, count):
         for i in range(count):
-            self.take_bias_image(test_type)
+            self.take_bias_image()
 
-    def create_fits_header_data(self, exposure, test_type, image_type):
-        data = {'ExposureTime': exposure, 'TestType': test_type, 'ImageType': image_type, 'TestSeqNum': self.test_seq_num}
+    def create_fits_header_data(self, exposure, image_type):
+        data = {'ExposureTime': exposure, 'TestType': self.test_type, 'ImageType': image_type, 'TestSeqNum': self.test_seq_num}
         if self.run:
             data.update({'RunNumber': self.run})
         return data
 
-    def take_bias_image(self, test_type):
-        return self.take_image(test_type, 'BIAS', 0, None)
+    def take_bias_image(self):
+        return self.__take_image(0, None, image_type='BIAS')
 
-    def take_image(self, test_type, image_type, exposure, expose):
-        fits_header_data = self.create_fits_header_data(exposure, test_type, image_type)
-        image_name, file_list = fp.takeExposure(fits_header_data, expose)
-        self.create_symlink(file_list, test_type.lower(), image_type.lower())
+    def symlink_test_type(self, test_type):
+        return test_type.lower()
+
+    def symlink_image_type(self, image_type):
+        return image_type.lower()
+
+    def take_image(self, exposure, expose_command, image_type=None, symlink_image_type=None):
+        return self.__take_image(exposure, expose_command, image_type, symlink_image_type)
+
+    def __take_image(self, exposure, expose_command, image_type=None, symlink_image_type=None):
+        image_type = image_type if image_type else self.image_type
+        symlink_image_type = symlink_image_type if symlink_image_type else self.symlink_image_type(image_type)
+        fits_header_data = self.create_fits_header_data(exposure, image_type)
+        image_name, file_list = fp.takeExposure(expose_command, fits_header_data)
+        self.create_symlink(file_list, self.symlink_test_type(self.test_type), symlink_image_type)
         self.test_seq_num += 1
         return (image_name, file_list)
 
@@ -54,27 +67,27 @@ class TestCoordinator(object):
 class BiasTestCoordinator(TestCoordinator):
     ''' A TestCoordinator for taking only bias images '''
     def __init__(self, options):
-        TestCoordinator.__init__(options)
+        super(BiasTestCoordinator, self).__init__(options, 'BIAS', 'BIAS')
         self.count = options.getInt('count', 10)
 
     def take_images(self):
-        self.take_bias_images(self.count, 'BIAS')
+        self.take_bias_images(self.count)
 
 class BiasPlusImagesTestCoordinator(TestCoordinator):
     ''' Base class for all tests that involve n bias images per test image'''
-    def __init__(self, options):
-        TestCoordinator.__init__(options)
+    def __init__(self, options, test_type, image_type):
+        super(BiasPlusImagesTestCoordinator, self).__init__(options, test_type, image_type)
         self.bcount = int(options.get('bcount', '1'))
 
-    def take_image(self, test_type, image_type, exposure, expose):
-        super(BiasPlusImagesTestCoordinator, self).take_bias_images(self.bcount, test_type)
-        super(BiasPlusImagesTestCoordinator, self).take_image(test_type, image_type, exposure, expose)
+    def take_bias_plus_image(self, exposure, expose_command, image_type=None, symlink_image_type=None):
+        super(BiasPlusImagesTestCoordinator, self).take_bias_images(self.bcount)
+        super(BiasPlusImagesTestCoordinator, self).take_image(exposure, expose_command, image_type, symlink_image_type)
 
 class DarkTestCoordinator(BiasPlusImagesTestCoordinator):
     ''' A TestCoordinator for darks '''
     def __init__(self, options):
-        BiasPlusImagesTestCoordinator.__init__(options)
-        self.darks = options.get('dark').replace('\n', '').split(',')
+        super(DarkTestCoordinator, self).__init__(options, 'DARK', 'DARK')
+        self.darks = options.getList('dark')
 
     def take_images(self):
         for dark in self.darks:
@@ -84,110 +97,126 @@ class DarkTestCoordinator(BiasPlusImagesTestCoordinator):
             expose_command = lambda: time.sleep(integration)
 
             for d in range(count):
-                self.take_image('DARK', 'DARK', expose_command, integration)
+                self.take_bias_plus_image(integration, expose_command)
 
-class FlatFieldTestCoordinator(TestCoordinator):
-    ''' A TestCoordinator for all tests that involve taking flat pairs with the flat field generator '''
-    def __init__(self, options):
-        super(FlatFieldTestCoordinator, self).__init__(options)
+class FlatFieldTestCoordinator(BiasPlusImagesTestCoordinator):
+    ''' A TestCoordinator for all tests that involve taking flatS with the flat field generator '''
+    def __init__(self, options, test_type, image_type):
+        super(FlatFieldTestCoordinator, self).__init__(options, test_type, image_type)
         self.bcount = int(options.get('bcount', '1'))
-        self.flats = options.get('flat').replace('\n', '').split(',')
 
-    def take_image(self, test_type, image_type, exposure, expose):
-        pd_readout = PhotodiodeReadout(exposure)
-        pd_readout.start_accumulation()
-        image_name, file_list = super(FlatFieldTestCoordinator, self).take_image(test_type, image_type, exposure, expose)
-        # TODO: Why does this need the last two arguments?
-        pd_readout.write_readings(file_list.getCommonParentDirectory().toString(), self.test_seq_num)
-
-    def take_image_pair(self, test_type, image_type, exposure, expose, nd_filter, wl_filter):
+    def set_filters(self, nd_filter, wl_filter):
         bot_bench.setNDFilter(nd_filter)
         bot_bench.setColorFilter(wl_filter)
-        super(FlatFieldTestCoordinator, self).take_bias_images(self.bcount, test_type)
-        for pair in range(2):
-            self.take_image(test_type, image_type, exposure, expose)
+
+    def take_image(self, exposure, expose_command, image_type=None, symlink_image_type=None):
+        pd_readout = PhotodiodeReadout(exposure)
+        pd_readout.start_accumulation()
+        image_name, file_list = super(FlatFieldTestCoordinator, self).take_image(exposure, expose_command, image_type, symlink_image_type)
+        # TODO: Why does this need the last two arguments?
+        pd_readout.write_readings(file_list.getCommonParentDirectory().toString(), self.test_seq_num)
 
     def compute_exposure_time(self, nd_filter, wl_filter, e_per_pixel):
         # TODO: Use per-filter config file to compute exposure
         return float(e_per_pixel)/100.0
 
-class FlatTestCoordinator(FlatFieldTestCoordinator):
+class FlatPairTestCoordinator(FlatFieldTestCoordinator):
     ''' A TestCoordinator for flat pairs'''
     def __init__(self, options):
-        super(FlatTestCoordinator, self).__init__(options)
+        super(FlatPairTestCoordinator, self).__init__(options, 'FLAT', 'FLAT')
         self.wl_filter = options.get('wl')
+        self.flats = options.getList('flat')
 
     def take_images(self):
         for flat in self.flats:
             e_per_pixel, nd_filter = flat.split()
+            e_per_pixel = float(e_per_pixel)
             exposure = self.compute_exposure_time(nd_filter, self.wl_filter, e_per_pixel)
             expose_command = lambda: bot_bench.openShutter(exposure)
             print "exp %s filter %s" % (exposure, nd_filter)
-            # TODO: test-type = %s_%s_flat%d' % (wl,e_per_pixel,pair)
-            self.take_image_pair('FLAT', 'FLAT', exposure, expose_command, nd_filter, self.wl_filter)
+            self.set_filters(nd_filter, self.wl_filter)
+            self.take_bias_images(self.bcount)
+            for pair in range(2):
+                self.take_image(exposure, expose_command, symlink_image_type='%s_%s_flat%d' % (self.wl_filter, e_per_pixel, pair))
 
 class SuperFlatTestCoordinator(FlatFieldTestCoordinator):
     def __init__(self, options):
-        super(SuperFlatTestCoordinator, self).__init__(options)
-        self.sflats = options.get('sflat').replace('\n', '').split(',')
+        super(SuperFlatTestCoordinator, self).__init__(options, 'SFLAT', 'FLAT')
+        self.sflats = options.getList('sflat')
+
+    def approx_equals(self, a, b):
+        return (a-b)/(a+b) < .2
+
+    def low_or_high(self, e_per_pixel):
+        if self.approx_equals(e_per_pixel, 1000):
+            return 'L'
+        elif self.approx_equals(e_per_pixel, 50000):
+            return 'H'
+        else:
+            return '?'
 
     def take_images(self):
         for sflat in self.sflats:
             wl_filter, e_per_pixel, count, nd_filter = sflat.split()
+            count = int(count)
+            e_per_pixel = float(e_per_pixel)
             exposure = self.compute_exposure_time(nd_filter, wl_filter, e_per_pixel)
             expose_command = lambda: bot_bench.openShutter(exposure)
-            #TODO: 'flat_%s_%s'% (wlFilter,e_per_pixel)
-            #TODO: use count?
-            self.take_image_pair('SFLAT', 'flat_%s_%s' % (wl_filter, e_per_pixel), exposure, expose_command, nd_filter, wl_filter)
+            self.set_filters(nd_filter, wl_filter)
+            for c in range(count):
+                self.take_bias_plus_image(exposure, expose_command, symlink_image_type='flat_%s_%s'% (wl_filter, self.low_or_high(e_per_pixel)))
 
 class LambdaTestCoordinator(FlatFieldTestCoordinator):
     def __init__(self, options):
-        super(LambdaTestCoordinator, self).__init__(options)
+        super(LambdaTestCoordinator, self).__init__(options, 'LAMBDA', 'FLAT')
         self.imcount = int(options.get('imcount', 1))
-        self.lambdas = options.get('lambda').replace('\n', '').split(',')
+        self.lambdas = options.getList('lambda')
 
     def take_images(self):
         for lamb in self.lambdas:
             wl_filter, e_per_pixel, nd_filter = lamb.split()
             exposure = self.compute_exposure_time(nd_filter, wl_filter, e_per_pixel)
             expose_command = lambda: bot_bench.openShutter(exposure)
-            #TODO: 'flat_%s_%s' % (wlFilter,e_per_pixel)
-            self.take_image_pair('LAMBDA', 'FLAT', exposure, expose_command, nd_filter, wl_filter)
+            self.set_filters(nd_filter, wl_filter)
+            self.take_bias_plus_image(exposure, expose_command, symlink_image_type='flat_%s_%s' % (wl_filter, e_per_pixel))
 
 class Fe55TestCoordinator(FlatFieldTestCoordinator):
     def __init__(self, options):
-        super(Fe55TestCoordinator, self).__init__(options)
+        super(Fe55TestCoordinator, self).__init__(options, 'FE55_FLAT', 'FE55')
+        self.flats = options.getList('flat')
         (fe55exposure, fe55count) = options.get('count').split()
         self.fe55exposure = float(fe55exposure)
         self.fe55count = int(fe55count)
 
-    def create_fits_header_data(self, exposure, test_type, image_type):
-        data = super(Fe55TestCoordinator, self).create_fits_header_data(exposure, test_type, image_type)
-        data.update({'ExposureTime2': self.fe55exposure})
+    def create_fits_header_data(self, exposure, image_type):
+        data = super(Fe55TestCoordinator, self).create_fits_header_data(exposure, image_type)
+        if image_type != 'BIAS':
+            data.update({'ExposureTime2': self.fe55exposure})
         return data
 
     def take_images(self):
         for flat in self.flats:
             wl_filter, e_per_pixel = flat.split()
             #TODO: None? which ND filter to use?
-            exposure = self.compute_exposure_time(None, wl_filter, e_per_pixel)
+            nd_filter = 'None'
+            exposure = self.compute_exposure_time(nd_filter, wl_filter, e_per_pixel)
             print "exp %s filter %s" % (exposure, wl_filter)
             def expose_command():
                 bot_bench.openShutter(exposure) # Flat
                 bot_bench.openFe55Shutter(self.fe55exposure) # Fe55
             #TODO: The total exposure time is needed by photodiode?
-            #TODO: test_type should be '%s_flat_%s' % (filter,e_per_pixel)
-            self.take_image_pair('FE55_FLAT', 'FE55', exposure, expose_command, None, wl_filter)
+            self.set_filters(nd_filter, wl_filter)
+            self.take_bias_plus_image(exposure, expose_command, symlink_image_type='%s_flat_%s' % (wl_filter, e_per_pixel))
 
 class CCOBTestCoordinator(BiasPlusImagesTestCoordinator):
     def __init__(self, options):
-        super(CCOBTestCoordinator, self).__init__()
+        super(CCOBTestCoordinator, self).__init__(options, 'CCOB', 'CCOB')
         self.imcount = int(options.get('imcount', '1'))
         xoffset = float(options.get('xoffset'))
         yoffset = float(options.get('yoffset'))
         bot.setLampOffset(xoffset, yoffset)
-        self.exposures = options.get('expose').replace('\n', '').split(',')
-        self.points = options.get('point').replace('\n', '').split(',')
+        self.exposures = options.getList('expose')
+        self.points = options.getList('point')
 
     def take_images(self):
         for point in self.points:
@@ -199,33 +228,28 @@ class CCOBTestCoordinator(BiasPlusImagesTestCoordinator):
                 duration = float(duration)
                 expose_command = lambda: ccob.fireLED(led, current, duration)
                 for i in range(self.imcount):
-                    # TODO:  '%s_%s_%s' % (led,x,y)
-                    self.take_image('CCOB', 'CCOB', duration, expose_command)
+                    self.take_bias_plus_image(duration, expose_command, symlink_image_type='%s_%s_%s' % (led, x, y))
 
 
 class XTalkTestCoordinator(BiasPlusImagesTestCoordinator):
     def __init__(self, options):
-        super(XTalkTestCoordinator, self).__init__()
+        super(XTalkTestCoordinator, self).__init__(options, 'XTALK', 'XTALK')
         self.imcount = int(options.get('imcount', '1'))
         xoffset = float(options.get('xoffset'))
         yoffset = float(options.get('yoffset'))
         bot.setLampOffset(xoffset, yoffset)
-        self.exposures = options.get('xtalk').replace('\n', '').split(',')
-        self.points = options.get('point').replace('\n', '').split(',')
+        self.exposures = options.getList('expose')
+        self.points = options.getList('point')
 
     def take_images(self):
         for point in self.points:
             (x, y) = [float(x) for x in point.split()]
             bot.moveTo(x, y)
             for exposure in self.exposures:
-                (led, current, duration) = exposure.split()
-                #TODO: Set LED
-                current = float(current)
-                duration = float(duration)
+                exposure = float(exposure)
                 expose_command = lambda: bot_bench.openShutter(exposure)
                 for i in range(self.imcount):
-                    # TODO:  '%s_%s_%s' % (x, y, exposure)
-                    self.take_image('XTALK', 'XTALK', duration, expose_command)
+                    self.take_bias_plus_image(exposure, expose_command, symlink_image_type='%s_%s_%s' % (x, y, exposure))
 
 
 def do_bias(options):
@@ -233,14 +257,14 @@ def do_bias(options):
     tc = BiasTestCoordinator(options)
     tc.take_images()
 
-def do_darks(options):
-    print "darks called %s" % options
+def do_dark(options):
+    print "dark called %s" % options
     tc = DarkTestCoordinator(options)
     tc.take_images()
 
 def do_flat(options):
     print "flat called %s" % options
-    tc = FlatTestCoordinator(options)
+    tc = FlatPairTestCoordinator(options)
     tc.take_images()
 
 # This is what Seth's document calls fe55+flat, but Aaron's config calls FE55
@@ -272,4 +296,3 @@ def do_xtalk(options):
     print "xtalk called %s" % options
     tc = XTalkTestCoordinator(options)
     tc.take_images()
-    
