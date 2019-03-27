2cdd14d1b8a9cfa2d70c81c5d6a1648c6c7f0319
Tony Johnson <tonyj@stanford.edu>
2019-03-27 08:57:16 -0700

    Initial implementation of scan mode (LSSTTD-1385) -- UNTESTED

--- lib/fp.py
+++ lib/fp.py
@@ -67,6 +67,58 @@ def takeCombinedExposure(exposeCommand=None, fitsHeaderData=None, secondExposeCo
      fp.waitForImages(imageTimeout)
      return (imageName, None)   
 
-      
-   
+def rafts():
+   #TODO: Should not be hardwired
+   return [fp.R10(), fp.R22() ]
 
+def rebs():
+   result = []
+   for r in rafts():
+      result += [r.Reb0(), r.Reb1(), r.Reb2()]
+   return result
+
+def aspics():
+   result = []
+   for r in rebs():
+      result += [r.ASPIC0(), r.ASPIC1(), r.ASPIC2(), r.ASPIC3(), r.ASPIC4(), r.ASPIC5() ]
+   return result
+
+def getSequencerParameter(name):
+   params = []
+   for raft in rafts():
+      params += raft.getSequencerParameter(name)
+      unique = set(params)
+      if len(unique)!=1:
+         raise "Inconsistent sequencer parameters "+params
+      return unqiue.pop()
+
+def setSequencerParameter(name, value):
+   for raft in rafts():
+      raft.setSequencerParameter(name, value)
+
+def isScanMode():
+   result = []
+   for reb in rebs():
+      result += reg.getRegister(0x330000,1)
+   unique = set(result)
+   if len(unique)!=1:
+      raise "Inconsistent scan mode "+result
+   return unique.pop()
+
+def setScanMode(value):
+   for reb in rebs():
+      reb.setRegister(0x330000, 1 if value else 0) 
+
+def isTransparentMode():
+   result = []
+   for aspic in aspics():
+      result += aspic.getTm()
+   unique = set(result)
+   if len(unique)!=1:
+      raise "Inconsistent transparent mode "+result
+   return unique.pop()   
+
+def setTransparentMode(value):
+   for aspic in aspics():
+      aspic.change(tm,  1 if value else 0)      
+   
