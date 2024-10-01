fad2138825037a0a99f43a9d6490c385c0d11f4c
seanmacb <seanmacb@umich.edu>
2024-10-01 13:08:14 +0000

    make sure fp-script closes the shutter regadless of whether success or not

--- bot-data.py
+++ bot-data.py
@@ -9,7 +9,7 @@ from ccs import versions
 from ccs import configs
 from java.lang import Boolean
 from java.time import Duration
-import fp
+import fp as libfp
 
 # Temporary work around for problems with CCS responsiveness
 CCS.setDefaultTimeout(Duration.ofSeconds(30))
@@ -47,8 +47,14 @@ if options.run:
 
 import config
 
-cfg = config.parseConfig(args[0])
-config.execute(cfg, {"dry_run": options.dry_run, "run": options.run, "symlink": options.symlink, "skip": options.skip, "limit": options.limit})
 
-fp.checkShutterStatus("CLOSE")
+try:
+    cfg = config.parseConfig(args[0])
+    config.execute(cfg, {"dry_run": options.dry_run, "run": options.run, "symlink": options.symlink, "skip": options.skip, "limit": options.limit})
+
+except:
+    raise
+
+finally
+    libfp.checkShutterStatus("CLOSE")
 
